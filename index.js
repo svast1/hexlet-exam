225dd1d4698ffeef3d764117dfef814e38ff258b
svast1 <dubboylbox@mail.ru>
2024-06-26 10:24:34 +0300

    done

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(3000);
+  console.log('Express started on port 3000');
+}
